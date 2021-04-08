4250450317826bdd3b68f87e9f4464b36d019a65
mwdoyle <mwdoyle@sfu.ca>
2021-04-08 11:19:56 -0700

    fix typo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
       <div>
         <h1>Hello, My name is Matthew Doyle</h1>
-        <h1>I am a Software Enigineer in Vancouver British Columbia</h1>
+        <h1>I am a Software Engineer in Vancouver British Columbia</h1>
         <img src="/images/profilePicture.png" alt=""/>
         <h1>About:</h1>
         <h2>In progress...</h2>
