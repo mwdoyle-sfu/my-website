fa25206c2eb05995709e6c59c3aa03a8aea7991a
mwdoyle <mwdoyle@sfu.ca>
2022-04-19 12:38:42 -0700

    add about

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ const App = () => {
   return (
       <div>
         <h1>Hello, My name is Matthew Doyle</h1>
-        <h1>I am a Software Engineer in Vancouver British Columbia</h1>
+        <h1>I am a Software Engineer in Vancouver, British Columbia, Canada</h1>
         <img src="/images/profilePicture.png" alt=""/>
         <h1>About:</h1>
-        <h2>In progress...</h2>
+        <h2>BSc. Computing Science - Software Systems</h2>
         <h1>News:</h1>
         <h2>In progress...</h2>
       </div>
